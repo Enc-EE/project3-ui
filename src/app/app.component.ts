dd4a70dc9550611bc7dc681257041961ed8ae497
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2018-07-30 22:34:19 +0200

    login with google test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare let gapi: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project3-ui';
+  private auth2: any;
+
+  login = () => {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '...',
+        scope: 'email'
+      });
+      this.auth2.signIn().then(() => {
+        if (this.auth2.isSignedIn.get()) {
+          let profile = this.auth2.currentUser.get().getBasicProfile();
+          let authResponseObj = this.auth2.currentUser.get().getAuthResponse(true);
+          console.log(profile.getId());
+          console.log(profile.getName());
+          console.log(profile.getEmail());
+          console.log(profile.getImageUrl());
+          console.log(authResponseObj.access_token);
+          console.log(authResponseObj.id_token);
+        }
+      });
+    });
+  }
 }
