b73404fd8587b7ed9d820459eb726dc8eafbcb76
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2018-08-14 19:17:17 +0200

    added angular material :O

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ListRepository } from './repositories/listRepository';
@@ -12,6 +13,7 @@ import { RouterModule } from '../../node_modules/@angular/router';
 import { ListItemComponent } from './list-item/list-item.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MatButtonModule, MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,10 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    MatButtonModule,
+    MatSelectModule,
     HttpClientModule,
     RouterModule.forRoot([]),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
